6b712e1439f9e915b7979580f3f3b4857fd57ed1
gerardPF <gerard.pf@hotmail.com>
2018-04-09 09:34:37 +0200

    soc guay

---
+++ src/Vista/SettingsViews/Settings.java
@@ -0,0 +1,13 @@
+package Vista;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Settings extends JFrame {
+    private CardLayout layout;
+    private SettingsView settingsView;
+    prvate
+
+
+
+}
