8dabb83962110ea12d0d90d981f69400e5b83586
Meritxell <mericiura1998@gmail.com>
2018-05-19 15:56:22 +0200

    Comentaris

--- src/Vista/View.java
+++ src/Vista/View.java
@@ -3,8 +3,12 @@ package Vista;
 import Controlador.Controller;
 
 import javax.swing.*;
-import java.awt.*;
 
+/**Classe abstracta creada per tal que totes les vistes hagin d'implementar el metode addController*/
 public abstract class View extends JPanel{
+    /**
+     * Metode que enllaça els diferents elements de la vista que realitzin accions amb el seu Listener
+     * @param c Controlador
+     */
     public abstract void addController(Controller c);
 }
