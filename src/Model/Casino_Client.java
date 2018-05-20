ae2eb8d947cced83c1eaa0c89bde2f0d23de4ce4
MSaula <miquelsaula@gmail.com>
2018-05-20 14:52:06 +0200

    comentat main

--- src/Model/Casino_Client.java
+++ src/Model/Casino_Client.java
@@ -6,6 +6,24 @@ import Network.*;
 import Vista.MainFrame.Finestra;
 import Controlador.SplashScreen;
 
+/**
+ * Classe principal del Client. Inicia les finestres necessaries,
+ * carrega la informació adient i inicia tot el joc.
+ *
+ * Concretament inicia en una primera instancia una finestra de càrrega
+ * que informa a l'usuari sobre els recursos que s'estàn carregant, els que
+ * consisteixen principalment en imatges i audios.
+ *
+ * Un cop carregada tota la informació, s'inicialitza la finestra principal del
+ * joc, juntament amb la barra superior personalitzada que permet arrossegar la
+ * finestra, tancar-la, minimitzar-la, fer full screen, silenciar els sons i
+ * la música i dins del joc, accedir als Settings de l'usuari.
+ *
+ * Juntament amb la finestra s'inicialitza el controlador, amb el que relitza el
+ * vincle que permetrà a l'usuari interaccionar amb la finestra. Paral·lelament
+ * es carrega el Network Manager, que consisteix en l'encarregat de controlar
+ * la comunicació amb el servidor.
+ */
 public class Casino_Client {
     public static void main(String[] args) {
 
