939767438dd3626b05d57a6e1a31d83f373976d9
MSaula <miquelsaula@gmail.com>
2018-05-20 14:43:36 +0200

    Comentat betList

--- src/Model/BetList.java
+++ src/Model/BetList.java
@@ -5,12 +5,15 @@ import Network.Message;
 /**
  * Missatge que serveix per a transmitir la informació
  * necessària per a representar totes les apostes realitzades pels
- * diferents usuaris.
+ * diferents usuaris. I anar actualitzant el seu contingut
  */
 public class BetList extends Message {
 
-    public static final int ROULETTE = 0;
-    public static final int HORSES = 1;
+    /** Tipus de missatge especific per a la ruleta */
+    private static final int ROULETTE = 0;
+
+    /** Tipus de missatge especific per als cavalls */
+    private static final int HORSES = 1;
 
     /** Context del missatge*/
     private String context;
@@ -18,8 +21,14 @@ public class BetList extends Message {
     /** Identificador del missatge*/
     private double ID;
 
+    /** Conjunt d'informació a mostrar al llistat d'usuaris i apostes */
     private String[][] info;
 
+    /**
+     * Constructor de la classe.
+     * @param info Nova llista a actualitzar
+     * @param type Tipus de joc al que correspon
+     */
     public BetList(String[][] info, int type) {
         if (type == ROULETTE) context = "rouletteListUpdate";
         else if (type == HORSES) context = "horsesListUpdate";
@@ -28,15 +37,18 @@ public class BetList extends Message {
         this.info = info;
     }
 
+    /** Getter de la informació a mostrar */
     public String[][] getInfo() {
         return info;
     }
 
+    /** Getter del context del missatge */
     @Override
     public String getContext() {
         return context;
     }
 
+    /** Getter del context de l'identificador del missatge */
     @Override
     public double getID() {
         return ID;
