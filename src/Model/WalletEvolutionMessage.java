82401f70d07f8e24ccd95488b2f05142fb8d946e
MSaula <miquelsaula@gmail.com>
2018-05-20 15:12:50 +0200

    comentaris varis

--- src/Model/WalletEvolutionMessage.java
+++ src/Model/WalletEvolutionMessage.java
@@ -5,8 +5,9 @@ import Network.Transmission;
 
 import java.util.LinkedList;
 
-/** Missatge orientat a la grafica de l'evolucio de la cartera de l'usuari*/
-
+/**
+ * Missatge orientat a la grafica de l'evolucio de la cartera de l'usuari
+ */
 public class WalletEvolutionMessage extends Message {
 
     /** Defineix el identificador del missatge. Util per quan s'envia amb el networkManager*/
@@ -18,6 +19,9 @@ public class WalletEvolutionMessage extends Message {
     /** Llista de la transactions del usuari*/
     private LinkedList <Transaction> transactions;
 
+    /**
+     * Constructor del missatge
+     */
     public WalletEvolutionMessage(){
         //Es defineix un identificador aleatori
         ID = Math.random();
@@ -29,20 +33,24 @@ public class WalletEvolutionMessage extends Message {
         transactions = new LinkedList<>();
     }
 
+    /** Getter del context del missatge */
     @Override
     public String getContext() {
         return context;
     }
 
+    /** Identificador del missatge */
     @Override
     public double getID() {
         return ID;
     }
 
+    /** Getter del llistat de transaccions realitzades per un usuari */
     public LinkedList<Transaction> getTransactions() {
         return transactions;
     }
 
+    /** Setter de les transaccions realitzades per un usuari */
     public void setTransactions(LinkedList<Transaction> transactions) {
         this.transactions = transactions;
     }
--- src/Network/Message.java
+++ src/Network/Message.java
@@ -4,12 +4,23 @@ import java.io.Serializable;
 
 /**
  * Pare de totes les comunicacions entre client - servidor
- * Obliga a que tots els elements que s'envien tinguin un ID
+ * Obliga a que tots els elements que s'envien tinguin un ID i
+ * un context.
+ *
+ * L'identifiador serveix per a facilitar el efecte ping-pong de la comunicació
+ * ja que al enviar un missatge al servidor, aquest en la majoria de casos ha de
+ * respondre, i per identificar la resposta en mig d'altres possibles missatges,
+ * s'utilita un identificador aleatori que es comparteix entre missatge enviat i resposta.
+ *
+ * Per altre banda, el context serveix per a identificar la finalitat del missatge,
+ * ja que tots els missatges s'envien per un mateix canal, el context és qui s'encarrega
+ * d'informar sobre si el missatge tracta sobre un intent de registrar-se l'usuari, o
+ * bé una aposta del joc dels cavalls, per exemple.
  */
-
 public abstract class Message implements Serializable {
-
+    /** Getter del context del missatge */
     public abstract String getContext();
-    public abstract double getID();
 
+    /** Getter de l'identificador del missatge */
+    public abstract double getID();
 }
